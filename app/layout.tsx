64e2a641052edb2b40001b9bedfa920cb65928ee
hey-hicode <kingdavies2021@gmail.com>
2025-05-23 15:51:40 +0100

    tt

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Goal Tracker ',
+  description: 'Created by King Davies ',
 }
 
 export default function RootLayout({
